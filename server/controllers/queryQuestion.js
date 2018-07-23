1543c9d329a56cd7a1d66740aef18ba0717cbbe9
Jono <jonoz@qq.com>
2018-07-23 18:05:51 +0800

    initPreviewQuestion

--- server/controllers/queryQuestion.js
+++ server/controllers/queryQuestion.js
@@ -5,11 +5,12 @@ module.exports = async ctx => {
     res.writeHead(200, {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "X-Requested-With",
-        "Access-Control-Allow-Methods": "POST,GET",
+        "Access-Control-Allow-Methods": "POST",
         "X-Powered-By": "3.2.1",
         'Content-Type': 'application/json;charset=utf-8'
     });
     var body = ctx.request.body
+    console.log(body)
     var paper_id = body.paper_id
     //查询
     var t_question = await mysql('t_question').select('*').where('paper_id', paper_id)
--- server/controllers/saveQuestion.js
+++ server/controllers/saveQuestion.js
@@ -2,8 +2,8 @@ const { mysql } = require('../qcloud')
 const cos = require('../qcos')
 const fs = require('fs')
 
-const que_sound = 'que_sound-1256378396'
-const que_image = 'que_image-1256378396'
+const que_sound = 'que-sound-1256378396'
+const que_image = 'que-image-1256378396'
 const Region = 'ap-guangzhou'
 
 module.exports = async ctx => {
@@ -22,7 +22,7 @@ module.exports = async ctx => {
     var fields = body.fields
     console.log(fields)
     var paper_id = fields.paper_id
-    var question_id = fields.question_id
+    var question_no = fields.question_no
     var title = fields.title
     var option_a = fields.option_a
     var option_b = fields.option_b
@@ -34,12 +34,14 @@ module.exports = async ctx => {
     var uploader = fields.uploader
     var article = fields.article
 
+    var point = 0
+    var question_id = paper_id + "_" + type + "_" + question_no
     //上传图片
     var src_image = ""
     function upload_image() {
         //接收图片文件
         var file_image = body.files.file_image
-        var image_name = paper_id + "_" + type + "_" + question_id + ".png"
+        var image_name = question_id + ".png"
         //file_image.name = image_name
         var params_image = {
             Bucket: que_image,
@@ -62,7 +64,7 @@ module.exports = async ctx => {
     function upload_sound() {
         //接收音频文件
         var file_sound = body.files.file_sound
-        var sound_name = paper_id + "_" + type + "_" + question_id + ".mp3"
+        var sound_name = question_id + ".mp3"
         var params_sound = {
             Bucket: que_sound,
             Region: Region,
@@ -80,38 +82,45 @@ module.exports = async ctx => {
     }
 
     if (type == "3"){
+        console.log(fields)
         //图片+声音
         await upload_image()
         await upload_sound()
     }else if (type == "4" || type == "5"){
+        console.log(fields)
         //只有声音
         await upload_sound()
     }
+    console.log(fields)
 
     result["src_sound"] = src_sound
     result["src_image"] = src_image
-
+    console.log(result)
     //数据库参数
-    var insert_values = [paper_id,question_id,title,option_a,option_b,option_c,option_d,right_option,an_explain,point,type,article,src_sound,src_image,uploader]
+    var insert_values = [question_id,paper_id,question_no,title,option_a,option_b,option_c,option_d,right_option,an_explain,point,type,article,src_sound,src_image,uploader]
+    console.log(insert_values)
     var update_values = [title,option_a,option_b,option_c,option_d,right_option,an_explain,article,src_sound,src_image]
+    console.log(update_values)
     var params_lst = insert_values.concat(update_values)
-    
+    console.log(params_lst)
     var str_sql = ""
     function get_sql(params_lst) {
-        var str_sql_ins = 'insert t_question (paper_id,question_id,title,option_a,option_b,option_c,option_d,right_option,an_explain,point,type,article,src_sound,src_image,uploader) values('
+        var str_sql_ins = 'insert t_question (question_id,paper_id,question_no,title,option_a,option_b,option_c,option_d,right_option,an_explain,point,type,article,src_sound,src_image,uploader) values('
         var que_m = new Array(params_lst.length)
         que_m.fill("?")
         var que_str = que_m + ""
         var str_sql_upd = ')on duplicate key update title=?,option_a=?,option_b=?,option_c=?,option_d=?,right_option=?,an_explain=?,article=?,src_sound=?,src_image=?'
+        console.log(que_str)
         return new Promise((resolve, reject) => {
             str_sql = str_sql_ins+que_str+str_sql_upd
+            console.log(str_sql)
             resolve()
         })
     }
 
-    await get_sql(params_lst)
+    await get_sql(insert_values)
     console.log(str_sql)
-    await mysql.raw(str_sql, params_lst);
-
+    await mysql.raw(str_sql, params_lst)
+    console.log(result)
     ctx.state.data = result
 }
